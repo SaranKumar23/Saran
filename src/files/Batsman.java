a2a68a2857355638bdf80b7f3b8b6b7fbd653b2c
SaranKumar23 <sarankumarc23@gmail.com>
2022-09-11 22:20:25 +0530

    saran

--- src/files/Batsman.java
+++ src/files/Batsman.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Batsman implements Serializable {
     private static final long serialVersionUID = -1330661014198608131L;
-    private String Name;ygyftfffftufuff
+    private String Name;
     private int runs;
     private int balls;
     private int fours;
